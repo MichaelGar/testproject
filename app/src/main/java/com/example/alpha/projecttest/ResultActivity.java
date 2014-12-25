7ef48c10ac79989f6f7fd9e7ce8f1435e1c973d9
Евгений <Vbymrby@yandex.ru>
2014-12-25 11:50:06 +0300

    Накидал активити с результатами(нужно поправить шрифты по AndroidWay)

--- app/src/main/java/com/example/alpha/projecttest/ResultActivity.java
+++ app/src/main/java/com/example/alpha/projecttest/ResultActivity.java
@@ -1,18 +1,41 @@
 package com.example.alpha.projecttest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class ResultActivity extends Activity {
-
+Button repeat, back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        repeat=(Button)findViewById(R.id.repeat);
+        back=(Button)findViewById(R.id.back);
+        repeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentrepeat = new Intent(ResultActivity.this, QuestionActivity.class);
+                //intentrepeat.putExtra("ID",lng);
+                startActivity(intentrepeat);
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentback = new Intent(ResultActivity.this, TestList.class);
+                //intentback.putExtra("login",((EditText) findViewById(R.id.editTextName)).getText().toString());
+                //intentback.putExtra("password",((EditText) findViewById(R.id.editTextPswd)).getText().toString());
+                startActivity(intentback);
+            }
+        });
     }
 
 
